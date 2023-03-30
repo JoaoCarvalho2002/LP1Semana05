4ee53ebfaf5b079c7b1c0b1d4eef5a72115c1bbf
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-30 18:06:28 +0100

    Exercicio 4, Completo

---
+++ Tempo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(300);
+            crono1.Stop();
+            crono2.Stop();
+            TimeSpan cro1 = crono1.Elapsed;
+            TimeSpan cro2 = crono2.Elapsed;
+            Console.WriteLine(cro1);
+            Console.WriteLine(cro2);
+        }
+
+    }
+}
