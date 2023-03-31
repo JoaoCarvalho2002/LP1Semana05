0a3a9a4ea3a2b5765a6a18c63f21da1d15a0fe22
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-31 15:58:11 +0100

    Exercicio 9, Tudo maiusculas, ordenado alfabeticamente e returnNO ARGUMENTS se nao forem dados argumentos

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+using System.Text.RegularExpressions;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          if (args.Length== 0){
+              Console.WriteLine("NO ARGUMENTS");
+
+
+          }else{
+              Array.Sort(args);
+              foreach(string i in args){
+              string word= i.ToUpper();
+
+              
+
+              Console.Write(word);
+              
+
+          }
+
+
+
+
+
+          }
+         
+
+
+
+
+
+        }
+    }
+}
