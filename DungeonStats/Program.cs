46799b109174039b4038fd0a6bc118506e2bf771
JoaoCarvalho2002 <jonitkick@gmail.com>
2026-03-19 16:22:55 +0000

    Methods add

---
+++ DungeonStats/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Common;
+
+namespace DungeonStats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+            
+        }
+        private static int Damage(int attack, int defense)
+        {
+            return attack - defense;
+        }
+        private static int Damage(int attack)
+        {
+            return attack;
+        }
+        private static int CriticalHit(int damage)
+        {
+         
+            if (damage<=0)
+            {
+                return 0;
+            }
+            else
+            {
+                return CriticalHit(damage+1);
+            }
+
+
+        }
+    }
+}
